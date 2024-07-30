e4bfc3d038017a25d39981562e1227b45d219f7f
Nicolas Merlos <140738721+SimpdeDIO@users.noreply.github.com>
2024-07-30 13:04:57 -0300

    Add files via upload

--- main page/main.js
+++ main page/main.js
@@ -3,17 +3,16 @@ const abrir_btn = document.getElementById("abrir-menu")
 
 
 function abrir(){
+  
+     nav.classList.add("visible");
 
-    if (nav.classList.contains("visible")) {
-              nav.classList.remove("visible");
-              abrir_btn.classList.remove("abierto")
-    }
-    else{
-        nav.classList.add("visible");
-        abrir_btn.classList.add("abierto");
-
-    }
     
+}
+
+function cerrar(){
+
+    nav.classList.remove("visible");
+
     
 }
 
