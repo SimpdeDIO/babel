00912f30dee4ddc6ed56c68b91a32682e1c3db9b
Nicolas Merlos <140738721+SimpdeDIO@users.noreply.github.com>
2024-07-31 20:43:50 -0300

    Add files via upload

---
+++ babel 31 - 7/traductor/traductor.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector("nav")
+const abrir_btn = document.getElementById("abrir-menu")
+
+
+function abrir(){
+  
+     nav.classList.add("visible");
+
+    
+}
+
+function cerrar(){
+
+    nav.classList.remove("visible");
+
+    
+}
