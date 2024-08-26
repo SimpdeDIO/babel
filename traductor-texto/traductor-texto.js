ad56118c051ccd18c93d3c004d4243079a8e587c
Nicolas Merlos <140738721+SimpdeDIO@users.noreply.github.com>
2024-08-26 17:07:15 -0300

    Add files via upload

--- traductor-texto/traductor-texto.js
+++ traductor-texto/traductor-texto.js
@@ -16,8 +16,99 @@ function cerrar(){
     
 }
 
-// function cerrar(){
-    
-//     nav.classList.remove("visible")
-// }
 
+function traducir(){
+    
+const apiUrl = 'https://translation.googleapis.com/language/translate/v2';
+
+const form = document.getElementById("texto-cont");
+
+const q = document.getElementById("textoUsuario");
+
+const result = document.getElementById("result");
+
+console.log("Cargo: " + q.value);
+
+// esto es lo que carga el usuario //
+
+
+
+const data = {
+
+  q: q.value,
+
+  source: 'en',
+
+  target: 'es'
+
+};
+
+
+
+// estto carga los headers//
+
+
+
+const requestOptions = {
+
+  method: 'POST',
+
+  headers: {
+
+    'Content-Type': 'application/json',
+
+	'X-goog-api-key': 'AIzaSyBahhDaOWo0LEBjDFZP_Lfvc2GoL6uM2hY'
+
+  },
+
+  //esto carga el body //
+
+  body: JSON.stringify(data),
+
+};
+
+console.log("Data: " + data);
+
+//envia el comando//
+
+fetch(apiUrl, requestOptions)
+
+  .then(response => {
+
+    if (!response.ok) { 
+
+	// si devuelve un http distito de 200 es por que dio error, tienen que definir el mensaje//
+
+      throw new Error('Oiga ponga bien las cosas');
+
+    }
+
+    // si devolvio un http 200 salio todo ok y retorna la respuesta //
+
+	return response.json();   
+
+  })
+
+ 
+
+  //esto te tranforma el json en cadena de text y que sera lo que le van a mostrar al usuario//
+
+  .then(data => {
+
+    
+
+    // console.log("Response: " + response.ok);
+    // result.textContent = JSON.stringify(data, null, 2);  
+
+  })
+
+  .catch(error => {
+
+    console.error
+
+
+
+('Error:', error);
+
+  });
+}
