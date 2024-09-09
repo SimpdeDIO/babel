713d6d2ce654596eed77eb6666465e10b39a0d85
Nicolas Merlos <140738721+SimpdeDIO@users.noreply.github.com>
2024-09-09 13:46:36 -0300

    Add files via upload

--- traductor/traductor.js
+++ traductor/traductor.js
@@ -71,3 +71,47 @@ function leerTexto(text) {
     speech.lang = currentLang;
     window.speechSynthesis.speak(speech);
 }
+
+function traducir() {
+    const apiUrl = 'https://translation.googleapis.com/language/translate/v2';
+    const q = texto.value;
+    const result = textArea;
+
+    console.log("Cargo: " + q);
+
+    // Datos para la solicitud
+    const data = {
+        q: q,
+        source: 'en',
+        target: 'es'
+    };
+
+    // Opciones de la solicitud
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-goog-api-key': ''
+        },
+        body: JSON.stringify(data)
+    };
+
+    console.log("Data: " + data.q);
+
+    // Enviar la solicitud
+    fetch(apiUrl, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ocurrió un error con la solicitud');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Response: " + data);
+            const translatedText = data.data.translations[0].translatedText;
+            result.textContent = translatedText;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
